f000ca9efc39d86fb5778b54250e63b5138295cd
Alina <alinakubikalex@gmail.com>
2024-09-23 19:38:02 +0200

    Добавляет декоративные элементы на главную (мобильная версия)

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,8 +1,8 @@
 /* в этот файл добавляет скрипты*/
 const navButton = document.querySelector('.header__nav-button');
-const navSection = document.querySelectorAll('.header__nav');
+const navSection = document.querySelector('.header__nav');
 
-navButton.onClick = function() {
+function onClicked() {
   if (navButton.classList.contains('header__nav-button--open')) {
     navSection.classList.remove('visually-hidden');
     navButton.classList.add('header__nav-button--close');
@@ -12,6 +12,6 @@ navButton.onClick = function() {
     navButton.classList.remove('header__nav-button--close');
     navButton.classList.add('header__nav-button--open');
   }
-};
+}
 
-
+navButton.addEventListener('click', onClicked);
